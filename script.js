f3a2c131ff1b435bc075e25fe145542a6a6f588c
gSUz92nc <99101158+gSUz92nc@users.noreply.github.com>
2024-02-02 08:47:51 +0000

    Add flag background and update drawMenu function

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+let flag_bg;
+
 function setup() {
     createCanvas(windowWidth, windowHeight);
 }
@@ -13,6 +16,9 @@ let firstRun = false; // This variable is used to check if the game has been run
 
 // This function is called when the game is run for the first time and when the window is resized
 function initiliseVariable() {
+
+    flag_bg = loadImage("the_reapist.jpg");
+
     player.yPos = windowHeight - 65;
     player.xPos = windowWidth / 2;
 }
@@ -24,7 +30,7 @@ let player = {
     speed: 15 // pixels per frame
 }
 
-gameState = "menu"; // This variable stores the current state of the game, this is used to determine what to draw on the screen
+let gameState = "menu"; // This variable stores the current state of the game, this is used to determine what to draw on the screen
 
 // This function is called every frame
 function draw() {
@@ -41,26 +47,38 @@ function draw() {
 }
 
 function drawMenu() {
-    background(10);
-    fill(255);
+    background(flag_bg);
+    fill(66, 66, 255);
     textAlign(CENTER);
     textSize(50);
     text("Game (fix text)", windowWidth / 2, windowHeight / 2 - 150);
     drawButton("Play", windowWidth / 2, windowHeight / 2 - 50);
-}
+}   
 
 // This function draws a button with the given text and position
-function drawButton(buttonText, xPos, yPos) {
-    rectMode(CENTER);
-    fill(0);
-    rect(xPos, yPos, 100, 50);
-    fill(255);
-    textAlign(CENTER);
-    textSize(20);
-    text(buttonText, xPos, yPos);
+function drawButton() {
+    // Create a new button
+    var playButton = document.createElement("button");
+
+    // Set the button's text
+    playButton.innerHTML = "Play";
+
+    // Set the button's position and style
+    playButton.style.position = "absolute";
+    playButton.style.left = window.innerWidth / 2 + "px";
+    playButton.style.top = window.innerHeight / 2 + "px";
+
+    // Add an event listener to the button
+    playButton.addEventListener("click", function() {
+        // The "Play" button was pressed
+        gameState = "game";
+    });
+
+    // Add the button to the document
+    document.body.appendChild(playButton);
 }
 
-function gameLoop() {
+function gameLoop() {   
     playerMovement();
     drawPlayer(player.xPos, player.yPos);
 }
